211033f3846fc70a52fd3c2435dc78ecefffc45c
Aditya <adityaharde05@gmail.com.com>
2023-04-24 15:56:19 +0530

    sixth

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -21,11 +21,16 @@ public class DriverServiceImpl implements DriverService {
 	@Override
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
-		Driver driver=new Driver();
+		Driver driver = new Driver();
+
+		Cab cab = new Cab();
+		cab.setPerKmRate(10);
+		cab.setAvailable(true);
+
 		driver.setMobile(mobile);
 		driver.setPassword(password);
-		driver.getCab().setAvailable(true);
-		driver.getCab().setPerKmRate(10);
+		driver.setCab(cab);
+
 		driverRepository3.save(driver);
 	}
 
